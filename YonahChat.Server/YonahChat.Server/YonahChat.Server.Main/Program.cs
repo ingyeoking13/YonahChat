22a049a3f6587bcca751580da719c849ec3fe93c
ingyeoking13 <ingyeoking13@gmail.com>
2020-02-09 23:53:44 +0900

    Grpc Test Server works good.

--- YonahChat.Server/YonahChat.Server/YonahChat.Server.Main/Program.cs
+++ YonahChat.Server/YonahChat.Server/YonahChat.Server.Main/Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using YonahChat.Grpc.Messages;
+using GGrpc=Grpc.Core;
 
 namespace YonahChat.Server.Main
 {
@@ -6,7 +12,43 @@ namespace YonahChat.Server.Main
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const int Port = 9000;
+            var cacert = File.ReadAllText(@"ca.crt");
+            var cert = File.ReadAllText("server.crt");
+            var key = File.ReadAllText("server.key");
+            var keypair = new KeyCertificatePair(cert, key);
+            var sslCredentials = new SslServerCredentials(new List<KeyCertificatePair>
+            {
+                keypair
+            }, cacert, false);
+
+
+            GGrpc.Server server = new GGrpc.Server
+            {
+                Ports = { new ServerPort("127.0.0.1", Port, sslCredentials) },
+                Services = { YonahChat.Grpc.Messages.ChatEntryRPC.BindService(new ChatEntryRPCService()) }
+            };
+
+            server.Start();
+            Console.WriteLine("Starting server on Port" + Port);
+            Console.WriteLine("Press any key to stop..");
+            Console.ReadKey();
+
+            server.ShutdownAsync().Wait();
+        }
+    }
+
+    public class ChatEntryRPCService : ChatEntryRPC.ChatEntryRPCBase
+    {
+        public override async Task<ChatEntryListReply> SayChatEntry(ChatEntryListRequest request, ServerCallContext context)
+        {
+            Metadata md = context.RequestHeaders;
+            foreach (var entry in md)
+            {
+                Console.WriteLine(entry.Key + " : " + entry.Value);
+            }
+
+            return new ChatEntryListReply();
         }
     }
 }
---
+++ YonahChat.WPF.Repositories/RepositoriesModule.cs
@@ -0,0 +1,20 @@
+﻿using YonahChat.WPF.Repositories.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace YonahChat.WPF.Repositories
+{
+    public class RepositoriesModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+ 
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+    }
+}
