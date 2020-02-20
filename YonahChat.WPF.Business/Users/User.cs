d1062ab52b16ee62706f5589157ac0949867342f
ingyeoking13 <ingyeoking13@gmail.com>
2020-02-21 01:54:35 +0900

    user model create - tistory 일기쓰다가 ... 작업못ㅎ함 ㅋㅋㅋ

---
+++ YonahChat.WPF.Business/Users/User.cs
@@ -0,0 +1,16 @@
+﻿namespace YonahChat.WPF.Business.Users
+{
+    public class User : BusinessBase
+    {
+        // ID Can not
+        public int id { get; }
+        private string _userName { get; set; }
+
+        private string _nickName;
+        public string NickName
+        {
+            get { return _nickName; }
+            set { SetProperty(ref _nickName, value); }
+        }
+    }
+}
